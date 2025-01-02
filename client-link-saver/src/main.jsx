072dfff4cd9161d3ced874419fa83ce6699455e9
clamba15 <cl@customcraftsoftware.com>
2025-01-02 14:37:18 -0800

    add auth

--- client-link-saver/src/main.jsx
+++ client-link-saver/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App.jsx';
+
+const domain = 'auth0-domain';
+const clientId = 'auth0-client-id';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </Router>
+  </StrictMode>
+);
