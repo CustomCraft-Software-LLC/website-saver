080253d0552415e7597b3d96d7f24f9a28cf492b
clamba15 <cl@customcraftsoftware.com>
2025-01-08 23:47:57 -0800

    data transfer the error

--- server-link-saver/middleware/authMiddleware.js
+++ server-link-saver/middleware/authMiddleware.js
@@ -2,6 +2,14 @@ const { expressjwt: jwt } = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 require('dotenv').config();
 
+const jwksClient = jwksRsa({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  cache: true,
+  cacheMaxAge: 3600,  
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+});
+
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
