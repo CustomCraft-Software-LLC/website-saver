4d6e3ce4476d70649449d2e9b77d3c8ecf5a5da6
clamba15 <cl@customcraftsoftware.com>
2025-01-11 00:27:31 -0800

    new jwt

--- server-link-saver/middleware/authMiddleware.js
+++ server-link-saver/middleware/authMiddleware.js
@@ -1,10 +1,10 @@
 const { auth } = require('express-oauth2-jwt-bearer');
 require('dotenv').config();
 
-const jwtCheck = auth({
+const checkJwt = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_DOMAIN, 
   tokenSigningAlg: 'RS256'
 });
 
-module.exports = { jwtCheck };
+module.exports = { checkJwt };
