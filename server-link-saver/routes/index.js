d3f5a71fb2755367c210efa5a3f51337d1a11523
clamba15 <cl@customcraftsoftware.com>
2025-01-11 21:46:55 -0800

    debug

--- server-link-saver/routes/index.js
+++ server-link-saver/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { checkJwt, logDecodedJwt } = require('../middleware/authMiddleware.js');
 const linkController = require('../controllers/linkController.js');
 
-router.use(logDecodedJwt);
-
 router.get('/links', checkJwt, linkController.getLinks);
 router.post('/links', checkJwt, linkController.createLink);
 /*
