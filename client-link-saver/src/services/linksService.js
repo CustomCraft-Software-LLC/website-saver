d7b2c4dc0ab3b396b909498f627e54af020476b2
clamba15 <cl@customcraftsoftware.com>
2025-01-15 14:21:17 -0800

    service

--- client-link-saver/src/services/linksService.js
+++ client-link-saver/src/services/linksService.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://website-saver.onrender.com';
+const API_BASE_URL = 'https://website-saver.onrender.com/api/links';
 
 export const fetchLinks = async (token) => {
-  const response = await axios.get(`${API_BASE_URL}/api/links`, {
+  const response = await axios.get(`${API_BASE_URL}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const addLink = async (token, linkData) => {
-  const response = await axios.post(`${API_BASE_URL}/api/links`, linkData, {
+  const response = await axios.post(`${API_BASE_URL}`, linkData, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const updateLink = async (token, linkId, linkData) => {
-  const response = await axios.put(`${API_BASE_URL}/links/${linkId}`, linkData, {
+  const response = await axios.put(`${API_BASE_URL}/${linkId}`, linkData, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
 
 export const deleteLink = async (token, linkId) => {
-  const response = await axios.delete(`${API_BASE_URL}/links/${linkId}`, {
+  const response = await axios.delete(`${API_BASE_URL}/${linkId}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
